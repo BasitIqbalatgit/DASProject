a5776e5ffdc58201b531d46930561502d6a2e093
Fawad Iqbal <122690338+fawadeqbal@users.noreply.github.com>
2023-07-12 18:13:39 +0500

    Update DALManager.java

--- SIS/src/DAL/DALManager.java
+++ SIS/src/DAL/DALManager.java
@@ -2,6 +2,7 @@ package DAL;
 
 import common.UserDTO;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import model.Message;
 import model.MessageType;
@@ -16,9 +17,9 @@ public class DALManager {
 
     public DALManager() {
 //<<<<<<< Updated upstream
-        objConnection = new MySQLConnection("jdbc:mysql://localhost:3306/mydb", "root", "BasitIqbal@050");
+       // objConnection = new MySQLConnection("jdbc:mysql://localhost:3306/mydb", "root", "BasitIqbal@050");
 //=======
-//        objConnection = new MySQLConnection("jdbc:mysql://localhost:3306/mydb", "root", "Admin123$");
+        objConnection = new MySQLConnection("jdbc:mysql://localhost:3306/mydb", "root", "Admin123$");
 //>>>>>>> Stashed changes
         objAdder = new RecordsAdder();
     }
@@ -33,12 +34,50 @@ public class DALManager {
         }
     }
     
-    public void verifyUser(UserDTO objUser, Response objResponse){
-       System.out.println("hello i am in getUserResult in DAL Mangaer");
-         Connection dbConnection = objConnection.getConnection();
-         String query="SELECT * FROM user WHERE email= ? and password= ?";
-         ResultSet result=objReader.getUserResultFromQuery(query, dbConnection,objUser, objResponse);
-         objMapper.getUser(result,objResponse);
-        
+//    public void verifyUser(UserDTO objUser, Response objResponse){
+//       System.out.println("hello i am in getUserResult in DAL Mangaer");
+//         Connection dbConnection = objConnection.getConnection();
+//         String query="SELECT * FROM user WHERE email= ? AND password= ?";
+//         ResultSet result=objReader.getUserResultFromQuery(query, dbConnection,objUser, objResponse);
+//         objMapper.getUser(result,objResponse);
+//        
+//    }
+    public void verifyUser(UserDTO user, Response responseObj) {
+        Connection connection = objConnection.getConnection();
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            String query = "SELECT * FROM user WHERE email = ? AND password = ?";
+            statement = connection.prepareStatement(query);
+            statement.setString(1, user.getEmail());
+            statement.setString(2, user.getPassword());
+            resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                user.setRole(resultSet.getString(4));
+                responseObj.messagesList.add(new Message("Successfully Login", MessageType.Information));
+            } else {
+                responseObj.messagesList.add(new Message("Invalid credentials check your username and password", MessageType.Error));
+            }
+        } catch (Exception ex) {
+            responseObj.messagesList.add(new Message(ex.getMessage(), MessageType.Exception));
+
+        } finally {
+
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+
+            } catch (Exception ex) {
+                responseObj.messagesList.add(new Message(ex.getMessage(), MessageType.Exception));
+            }
+        }
     }
 }
